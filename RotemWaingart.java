69b0d518741d5d52059386378d28a0ca5a4b54c3
Rotem Waingart <Rotem.waingart@gmail.com>
2025-04-08 17:03:54 +0300

    Update RotemWaingart.java print message

--- RotemWaingart.java
+++ RotemWaingart.java
@@ -1,7 +1,7 @@
 public class RotemWaingart {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("Rotem Waingart... " + i);
+            System.out.println("Rotem Waingart, print after update " + i);
         }
     }
 }
